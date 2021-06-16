560937f288963dc7e74a32b421acdcca23365050
path_env <mankaldeep@gmail.com>
2021-06-16 18:22:57 +0200

    RTCSM Interface

--- slam_particlefilter/particle.py
+++ slam_particlefilter/particle.py
@@ -37,8 +37,14 @@ class Particle:
         return self.Est_X_t
     
     def scan_match(self, Est_X_t, Est_X_t_1,Meas_Z_t,Meas_Z_t_1):
-        GT,self.GT_Lst = self.SM.match_LS(Meas_Z_t.to_numpy().T, 
-                                         Meas_Z_t_1.to_numpy().T, 
+        #ICP
+        # GT,self.GT_Lst = self.SM.match(Meas_Z_t.to_numpy().T, 
+        #                                  Meas_Z_t_1.to_numpy().T, 
+        #                                  Est_X_t,
+        #                                  Est_X_t_1)
+        #RTCSM
+        GT,self.GT_Lst = self.SM.match(Meas_Z_t, 
+                                         Meas_Z_t_1, 
                                          Est_X_t,
-                                         Est_X_t_1)
+                                         Est_X_t_1)                                         
         return GT, self.GT_Lst
